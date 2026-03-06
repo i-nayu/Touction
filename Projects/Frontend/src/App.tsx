5d540689c7de0f28f4c8b931c4648fb264f0dd2c
toaru05 <toaru8079@gmail.com>
2026-03-06 16:34:23 +0900

    Done

--- Projects/Frontend/src/App.tsx
+++ Projects/Frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./Pages/Register/Register.tsx";
 import Tournament from "./Pages/Tournament/Tournament.tsx";
+import Auction from "./Pages/Auction/Auction.tsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Register />} />
         <Route path='/tournament' element={<Tournament />} />
+        <Route path='/auction' element={<Auction />} />
       </Routes>
     </BrowserRouter>
   )
